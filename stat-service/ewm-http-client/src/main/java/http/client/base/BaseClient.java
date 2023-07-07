d4d09ad89a72cb2dd2715babc68887799730f883
DevSMike <likashev.migha@yandex.ru>
2023-07-07 14:03:47 +0300

    feat: фикс кода BaseClient

--- stat-service/ewm-http-client/src/main/java/http/client/base/BaseClient.java
+++ stat-service/ewm-http-client/src/main/java/http/client/base/BaseClient.java
@@ -2,7 +2,7 @@ package http.client.base;
 
 import java.util.Map;
 
-import org.springframework.lang.Nullable;
+import lombok.NonNull;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -21,35 +21,27 @@ public class BaseClient {
     }
 
     protected <T> ResponseEntity<Object> post(String path, T body) {
-        return makeAndSendRequest(path, null, body);
+        return makeAndSendRequest(path, body);
     }
 
-    private <T> ResponseEntity<Object> makeAndSendRequest(String path, @Nullable Map<String, Object> parameters, T body) {
+    private <T> ResponseEntity<Object> makeAndSendRequest(String path, @NonNull T body) {
         HttpEntity<T> requestEntity = new HttpEntity<>(body);
-
         ResponseEntity<Object> serverResponse;
+
         try {
-            if (parameters != null) {
-                serverResponse = rest.exchange(path, HttpMethod.POST, requestEntity, Object.class, parameters);
-            } else {
-                serverResponse = rest.exchange(path, HttpMethod.POST, requestEntity, Object.class);
-            }
+            serverResponse = rest.exchange(path, HttpMethod.POST, requestEntity, Object.class);
         } catch (HttpStatusCodeException e) {
             return ResponseEntity.status(e.getStatusCode()).body(e.getResponseBodyAsByteArray());
         }
         return prepareGatewayResponse(serverResponse);
     }
 
-    private <T> ResponseEntity<Object> makeAndSendRequestWithoutBody(String path, @Nullable Map<String, Object> parameters) {
+    private <T> ResponseEntity<Object> makeAndSendRequestWithoutBody(String path, @NonNull Map<String, Object> parameters) {
         HttpEntity<T> requestEntity = new HttpEntity<>(null);
 
         ResponseEntity<Object> serverResponse;
         try {
-            if (parameters != null) {
-                serverResponse = rest.exchange(path, HttpMethod.GET, requestEntity, Object.class, parameters);
-            } else {
-                serverResponse = rest.exchange(path, HttpMethod.GET, requestEntity, Object.class);
-            }
+            serverResponse = rest.exchange(path, HttpMethod.GET, requestEntity, Object.class, parameters);
         } catch (HttpStatusCodeException e) {
             return ResponseEntity.status(e.getStatusCode()).body(e.getResponseBodyAsByteArray());
         }
